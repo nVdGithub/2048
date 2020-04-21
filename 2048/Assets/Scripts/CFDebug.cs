f1d26caf161a52054f69534182c6005a623869bd
navid <navidheidari.nvd@gmail.com>
2020-04-21 07:59:43 +0200

    2nd video done
    15'	random location for new tiles
    21'	using j,i for x,y (not i and j)
    22'	using system.linq
    27'	CFDebug
    32'	Timer

---
+++ 2048/Assets/Scripts/CFDebug.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CFDebug : MonoBehaviour {
+
+    List<CFDebugObject> debugObjects = new List<CFDebugObject>();
+
+    public Vector2 startingPosition = new Vector2(10,10);
+    public int verticalSpacing = 30;
+    public int horizontalSpacing = 300;
+    public int labelHeight = 20;
+    public int labelWidth = 100;
+
+    public int fontSize = 25;
+    public Color fontColor = Color.white;
+
+    public bool showBox = true;
+
+    public int padding = 40;
+
+    private GUIStyle gUIStyle = new GUIStyle();
+
+
+	private void OnGUI()
+	{
+        int numDebugObjects = debugObjects.Count;
+        int currentIteration = 0;
+
+        gUIStyle.fontSize = fontSize;
+        gUIStyle.normal.textColor = fontColor;
+
+        if (showBox)
+        {
+
+            GUI.Box(new Rect(startingPosition.x, startingPosition.y, labelWidth * 2 + horizontalSpacing + (padding * 2), debugObjects.Count * (verticalSpacing + labelHeight) + (padding * 2)), "CFDebug Ver 1.0");
+        }
+
+        foreach (CFDebugObject d in debugObjects) {
+
+            string desc = d.description;
+            string val = d.value;
+
+            int yPos = (int)startingPosition.y + padding + ((verticalSpacing + labelHeight) * currentIteration);
+
+            GUI.Label(new Rect(startingPosition.x + padding, yPos, labelWidth, labelHeight), desc, gUIStyle);
+            GUI.Label(new Rect(labelWidth + horizontalSpacing, yPos, labelWidth, labelHeight), val, gUIStyle);
+
+            currentIteration++;
+        }
+
+
+	}
+
+    public void Add (string description, string value, string label) {
+
+        CFDebugObject debugObject = new CFDebugObject();
+
+        debugObject.description = description;
+        debugObject.value = value;
+        debugObject.label = label;
+
+        //- Find if an object with the specified label already exists
+        int index = debugObjects.FindIndex(x => x.label == label);
+
+        if (index != -1) {
+
+            //- The item was found
+            debugObjects.RemoveAt(index);
+            debugObjects.Insert(index, debugObject);
+        
+        } else {
+
+            //- The item doesn't exist
+            debugObjects.Add(debugObject);
+        }
+
+    }
+
+    public void Clear () {
+
+        debugObjects.Clear();
+    
+    }
+
+    public int NextLocation () {
+
+        int numObjects = debugObjects.Count;
+
+        return numObjects;
+    }
+}
+
+public class CFDebugObject : MonoBehaviour {
+
+    public string description;
+    public string value;
+    public string label;
+}
--- 2048/Assets/Scripts/Game.cs
+++ 2048/Assets/Scripts/Game.cs
@@ -1,9 +1,20 @@
-﻿using System.Collections;
+﻿//using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Game : MonoBehaviour
 {
+
+    public static int gridWidth = 4, gridHeight = 4;
+
+    public static Transform[,] grid = new Transform[gridWidth, gridHeight];
+
+    public CFDebug debug;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +24,89 @@ public class Game : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CheckUserInput();
+
+        debug.Add("Current Time", Time.time.ToString(), "currenttime");
+    }
+
+
+    void CheckUserInput()
+    {
+        bool down = Input.GetKeyDown(KeyCode.DownArrow) , up = Input.GetKeyDown(KeyCode.UpArrow) , left = Input.GetKeyDown(KeyCode.LeftArrow) , right = Input.GetKeyDown(KeyCode.RightArrow);
+
+        if (down || up || left || right)
+        {
+
+            if (down) {
+                debug.Add("Player Pressed Key", "Down", "checkuserinput");
+
+                GetRandomLocationForNewTile();
+            }
+
+            if (up) {
+                debug.Add("Player Pressed Key", "Up", "checkuserinput");
+            }
+
+            if (left)
+            {
+                debug.Add("Player Pressed Key", "Left", "checkuserinput");
+            }
+
+            if (right)
+            {
+                debug.Add("Player Pressed Key", "Right", "checkuserinput");
+            }
+
+        }
+
+    }
+
+
+
+    Vector2 GetRandomLocationForNewTile()
+    {
+        List<int> x = new List<int>();
+        List<int> y = new List<int>();
+
+        for (int j = 0; j<gridWidth; j++)
+        {
+            for (int i = 0; i < gridHeight; i++)
+            {
+                if (grid[j,i] == null)
+                {
+                    x.Add(j);
+                    y.Add(i);
+                }
+            }
+
+        }
+
+
+        int randIndex = Random.Range(0,x.Count);
+
+        int randX = x.ElementAt(randIndex);
+        int randY = y.ElementAt(randIndex);
+
+        debug.Add("New Random Tile Location", randX + "," + randY, "randomlocation");
         
+
+
+        return new Vector2(randX, randY);
+
+    }
+
+
+
+
+
+    /// <summary>
+    /// Restart Game Play - video #2
+    /// </summary>
+
+    public void PlayAgain()
+    {
+
+        grid = new Transform[gridWidth, gridHeight];
+
     }
 }
